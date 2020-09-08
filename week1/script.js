c7de26d5630fe8f669e6e0c849ff39ba18f5fcd9
TTaylor777 <54610815+TTaylor777@users.noreply.github.com>
2020-09-07 23:58:48 -0700

    homework1

---
+++ week1/script.js
@@ -0,0 +1,6 @@
+var button1 = document.getElementById('contactbutton')
+var form = document.getElementById('form1')
+
+button1.addEventListener('click', function(){
+    form.style.display = 'block'
+})
